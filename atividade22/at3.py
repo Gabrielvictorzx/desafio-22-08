d4f908c1fb2ed67dfd341e9cdbc362640f72e0a6
rdiasss <rafaeldiasguerra123@outlook.com>
2024-08-22 08:30:53 -0300

    atividades

---
+++ atividade22/at3.py
@@ -0,0 +1,8 @@
+metros = float(input("Digite o valor em metros: "))
+cm = metros  * 100
+mm = metros * 1000
+km = metros/ 1000
+
+print(f"O valor em centimetros é: {cm:.2f} cm")
+print(f"O valor em milimetros é: {mm:.2f} mm")
+print(f"O valor em quilometros é: {km:.2f} km")
