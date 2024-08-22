6265c950c719ee219236dd39102f8c7ab71d4a34
rdiasss <rafaeldiasguerra123@outlook.com>
2024-08-22 08:43:37 -0300

    atividades

---
+++ at1.py
@@ -0,0 +1,7 @@
+celsius = int(input("Digite seu numero: "))
+kelvin = celsius + 273
+fahrenheit = 1.8 * celsius + 32
+
+print(f"A temperatura em Kelvin é: {kelvin:.2f}K")
+print(f"A temperatura em fahrenheit é: {fahrenheit:.2f}F")
+
